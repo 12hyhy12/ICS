e429ceb59ad29373bf3e4a9ab31ccc3ef909745f
ForeverZyh <zyhzyhzyh@pku.edu.cn>
2016-11-10 21:26:39 +0800

    cachelab

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -1,3 +1,5 @@
+//Zhang Yuhao
+//1500012703
 /* 
  * trans.c - Matrix transpose B = A^T
  *
@@ -27,6 +29,81 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     REQUIRES(M > 0);
     REQUIRES(N > 0);
 
+	int i,j;
+	
+	if (N==M&&N==32)
+	{
+		int t[8],x;
+		for(x=0;x<M;x+=8)
+		{
+			for(i=0;i<N;i++)
+			{
+				for(j=x+7;j>=x;j--) t[j-x]=A[i][j];
+				for(j=x+7;j>=x;j--) B[j][i]=t[j-x];
+			}
+		}
+	}
+	else if (N==M)
+	{
+		/*A[a-b][c-d]
+		B[c-d][a-b]
+		-------------------
+		A[a][c-d] M[c-d]<=8
+		B[c-d][a] M[a-b]<=16
+		A[a+1][c-d]
+		B[c-d][a+1]
+		
+		*/
+		int x,l;
+		for(x=0;x<M;x+=8)
+		{
+			for(i=0;i<N;i+=8)
+				if (i!=x)
+				{
+					int st;
+					for(st=0;st==x||st==i;st+=8);
+					for(l=0;l<4;l++)
+					{
+						for(j=0;j<4;j++) B[x+j][i+l]=A[i+l][x+j];
+						for(j=4;j<8;j++) B[st+j][st+l]=A[i+l][x+j];
+					}
+					for(l=4;l<8;l++)
+					{
+						for(j=4;j<8;j++) B[st+j][st+l]=A[i+l][x+j];
+						for(j=0;j<4;j++) B[x+j][i+l]=A[i+l][x+j];
+					}
+					for(l=4;l<8;l++)
+						for(j=0;j<8;j++) B[x+l][i+j]=B[st+l][st+j];
+				}
+		}
+		int t[4];
+		for(x=0;x<M;x+=4)
+		{
+			for(i=x/8*8;i<x/8*8+8;i++)
+			{
+				for(j=x+3;j>=x;j--) t[j-x]=A[i][j];
+				for(j=x+3;j>=x;j--) B[j][i]=t[j-x];
+			}
+		}
+		
+	}
+	else
+	{
+		int t1,t2,tmp,x,y;
+		for (i = 0; i < N; i+=21)
+		    for (j = 0; j < M; j+=23)
+		    {
+		    	t1=i+21;t2=j+23;
+		    	if (t1>N) t1=N;
+		    	if (t2>M) t2=M;
+		    	for (x=i;x<t1;x++)
+		    		for(y=j;y<t2;y++)
+		    		{
+		    			tmp=A[x][y];
+		    			B[y][x]=tmp;
+		    		}
+			}
+	}
     ENSURES(is_transpose(M, N, A, B));
 }
 
