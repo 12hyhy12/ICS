759724d476e9f249f344ae8c6c1d1355f9f6495b
ForeverZyh <zyhzyhzyh@pku.edu.cn>
2016-11-24 11:36:44 +0800

    complete hw8

--- tshlab-handout/test.c
+++ tshlab-handout/test.c
@@ -9,21 +9,22 @@ char* tfgets()
 	printf("Enter the maximum size of an input line:\n");
 	int n;
 	scanf("%d",&n);
-	Signal(SIGALRM,handler);
-	Alarm(5);
-	if (sigsetjmp(buf,1)) 
-	{
-		printf("ha ha time up!\n");
-		return NULL;
-	}
-	while (1);
 	char*ans;
 	ans=malloc(sizeof(char)*(n+2));
+	fgets(ans,n,stdin);//   get rid of /n after the number
+	printf("Please type your input line in 5 secs\n");
+	if (sigsetjmp(buf,1)) 
+	{
+		printf("\nHa ha time up!\n");
+		return NULL;
+	}
+	alarm(5);
 	fgets(ans,n,stdin);
 	return ans;
 }
 int main()
 {
+	Signal(SIGALRM,handler);
 	char*ans=tfgets();
 	if (ans!=NULL) 
 	{
